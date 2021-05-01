1cd4d0af7c3bc5218ee9f7dcda5ab60b6b890dc0
Sebastian Alsina <alsinas@me.com>
2021-05-01 04:46:19 -0400

    Better Hompage, _app Structured

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+export default function Footer() {
+    return (
+        <div className="p-12 py-24">
+            <div className="grid grid-cols-1 md:grid-cols-3">
+                <div>
+                    <p>Get Involved</p>
+                </div>
+                <div>
+                    <p>Contact us</p>
+                </div>
+                <div>
+                    <p>Contact us</p>
+                </div>
+            </div>
+        </div>
+    )
+}
